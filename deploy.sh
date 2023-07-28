849c58bb7d8f2ff91ea64dc1954798bd3de34d06
Gaëtan Cassiers <gaetan.cassiers@iaik.tugraz.at>
2023-07-28 15:28:44 +0200

    vendor theme

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-hugo -b https://www-crypto.elen.ucl.ac.be/crypto/
+hugo --gc --minify -b https://www-crypto.elen.ucl.ac.be/crypto/
 scp -r public/* www-data@perceval.elen.ucl.ac.be:/home/www-data/crypto_website
